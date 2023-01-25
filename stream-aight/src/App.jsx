75454a52f3c2b02fe1c21facddb41cf228bc5569
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-01-25 20:37:48 +0200

    feat - frontend

--- stream-aight/src/App.jsx
+++ stream-aight/src/App.jsx
@@ -1,34 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+
+// Helmet
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+// Images
+import StreamAILogo from "./assets/images/stream-ai-ght-transparent.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState("");
+  const [text, setText] = useState([]);
+
+  const handleMovieSearch = () => {
+    axios
+      .post("http://localhost:5000/api", { body: text })
+      .then((res) => {
+        console.log(res.data.data);
+        setData(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div className="App">
+    <HelmetProvider>
+      <Helmet>
+        <title>Hello World</title>
+        <link rel="canonical" href="https://www.stream-aight.ai/" />
+      </Helmet>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="mx-auto w-screen px-6">
+          <div className="py-4">
+            <div className="flex justify-start">
+              <a href="/">
+                <img
+                  src={StreamAILogo}
+                  className="h-36 w-auto"
+                  alt="Stream AIght Logo"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <div className="w-2/4	border border-black-800" />
+        </div>
+
+        <main className="mb-36">
+          <div className="container mx-auto m-12">
+            <h1 className="font-sans text-2xl font-bold text-center mb-4">
+              Let AI find your next movie or show
+            </h1>
+            <p className="font-sans text-2xl text-center px-24">
+              Enter the title of the movies or shows you last watched and
+              enjoyed. Please separate each title with a comma. The more titles
+              you add to the list, the better the recommendations.
+            </p>
+            <form
+              className="flex justify-center mt-8"
+              onSubmit={handleMovieSearch}
+            >
+              <input
+                className="p-0.5 w-2/5 border-2 border-black-600 rounded-tl-md	rounded-bl-md rounded-tr-none	rounded-br-none"
+                type="text"
+                name="title"
+                placeholder="Billions, Super Pumped, Ballers.."
+                onChange={(e) => setText(e.target.text)}
+              />
+              <button
+                className="bg-neutral-800 text-white rounded-bl-none rounded-tl-none"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="flex justify-center">
+            <p className="my-2">
+              <ul className="list-disc">
+                <li>{data.data}</li>
+              </ul>
+            </p>
+          </div>
+        </main>
+        <footer>
+          <div className="flex justify-center">
+            <div className="w-2/4	border border-black-800" />
+          </div>
+          <div className="p-4 m-4 flex justify-center">
+            <p>Built by: Masi</p>
+          </div>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </HelmetProvider>
+  );
 }
 
-export default App
+export default App;
